8b478557c386311cfffce1f943d3a24ac4f7e6aa
MOHD SHUAIB <shuaib6428@gmail.com>
2024-08-01 21:03:26 +0530

    check number is prime or not

---
+++ Coding Problems/1_NumberPrimeOrNot/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Check Number is Prime or not");
+
+Console.WriteLine("Enter number");
+int num = int.Parse(Console.ReadLine());
+
+int i;
+for(i = 2;i<num;i++){
+    if(num%i==0){
+        Console.WriteLine("{0} is Not a Prime number ",num);
+        break;
+    }
+}
+
+if(num == i){
+    Console.WriteLine("{0} is a prime number",num);
+}
