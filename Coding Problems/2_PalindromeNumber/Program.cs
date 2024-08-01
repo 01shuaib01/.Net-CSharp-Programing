d605d59125a52c019b837549ad530cc65b451b05
MOHD SHUAIB <shuaib6428@gmail.com>
2024-08-01 21:09:00 +0530

    Create Program.cs

---
+++ Coding Problems/2_PalindromeNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Check Number is Palindrom or not");
+
+Console.WriteLine("Enter number");
+int num = int.Parse(Console.ReadLine());
+
+int temp = num;
+int finalAns = 0;
+while(num!=0){
+    int rem = num % 10;
+    finalAns = finalAns * 10 + rem;
+    num = num / 10;
+}
+if(temp == finalAns){
+    Console.WriteLine("{0} is a Palindrome Number ",temp);
+}else{
+    Console.WriteLine("{0} is not a Palindrome number ",temp);
+}
+
+
+
