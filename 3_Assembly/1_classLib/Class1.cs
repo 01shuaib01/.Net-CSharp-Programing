123b3e5d96b55d1f24c7c02e7925c28bc1fa3b01
MOHD SHUAIB <shuaib6428@gmail.com>
2024-08-02 11:36:28 +0530

    How to make dll file

---
+++ 3_Assembly/1_classLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace _1_classLib;
+
+public class Class1
+{
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int sub(int a,int b){
+        return a-b;
+    }
+}
---
+++ 3_Assembly/project/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using _1_classLib;
+
+namespace project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the coding era !");
+            Class1 obj = new Class1();
+            int a = obj.add(12, 12);
+            int b = obj.sub(12, 5);
+
+            Console.WriteLine("Value of a -> " + a);
+            Console.WriteLine("Value of b -> " + b);
+        }
+    }
+}
