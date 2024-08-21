2f600765210f9fdfb52e1b9be466d4902900bcc5
MOHD SHUAIB <shuaib6428@gmail.com>
2024-08-21 11:02:25 +0530

    Create Program.cs

---
+++ Coding Problems/4_FindDuplicate/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program{
+    
+    // Time Complexity --> O(N2)
+    void findDuplicate(int[] arr){
+        for(int i = 0;i<arr.Length;i++){
+            for(int j = i+1;j<arr.Length;j++){
+                if(arr[j] == arr[i])
+                    Console.WriteLine(arr[i]);
+            }
+        }
+    }
+
+    // Time Complexity --> O(NLogN)
+    void findDuplicate2(int[] arr){
+        Array.Sort(arr);
+        for(int i = 0;i<arr.Length-1;i++){
+            if(arr[i] == arr[i+1]){
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+    public static void Main(string[] args)  {
+        int[] arr = {11, 22,11,4,55,6,4}   ; 
+        Program obj = new Program();
+        obj.findDuplicate(arr);
+        obj.findDuplicate2(arr);
+       
+       
+    }
+}
